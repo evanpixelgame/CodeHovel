8eddc8b321790f06aa8ad369f69a0be15bbab572
NatalyHelps <125290818+NatalyHelps@users.noreply.github.com>
2024-07-07 03:48:08 -0500

    much better

--- src/components/main/ThemeSelector.jsx
+++ src/components/main/ThemeSelector.jsx
@@ -8,8 +8,8 @@ const themeOptions = [
   "no-bars-light",
   "full-bars-dark",
   "full-bars-light",
-  "full-border-dark",
-  "full-border-light",
+  "full-border-(D)",
+  "full-border-(L)",
 ];
 
 function ThemeSelector() {
@@ -33,30 +33,35 @@ function ThemeSelector() {
           color: theme.color,
         }}
       >
-        <p>The current theme is {theme}</p>
-        <select value={theme} onChange={(e) => selectTheme(e.target.value)}>
-          <option value="light">Light</option>
-          <option value="dark">Dark</option>
-          <option value="no-bars-dark">No Bars Dark</option>
-          <option value="no-bars-light">No Bars Light</option>
-          <option value="full-bars-dark">Full Bars Dark</option>
-          <option value="full-bars-light">Full Bars Light</option>
-          <option value="full-border-dark">Full Border Dark</option>
-          <option value="full-border-light">Full Border Light</option>
-        </select>
-        <button id="themeButton" onClick={handleNextTheme}>
-          <span
-            style={{
-              fontSize: "10px",
-              position: "absolute",
-              transform: "translateX(-50%)",
-            }}
-          >
-            Click for
-          </span>
-          <br />
-          Next Theme
-        </button>
+        <p id="theme-text">
+          The current theme is: <br />
+          <span className="theme-name">{theme}</span>
+        </p>
+        <div id="buttonandselect">
+          <select value={theme} onChange={(e) => selectTheme(e.target.value)}>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="no-bars-dark">No Bars Dark</option>
+            <option value="no-bars-light">No Bars Light</option>
+            <option value="full-bars-dark">Full Bars Dark</option>
+            <option value="full-bars-light">Full Bars Light</option>
+            <option value="full-border-dark">Full Border D</option>
+            <option value="full-border-light">Full Border L</option>
+          </select>
+          <button id="themeButton" onClick={handleNextTheme}>
+            <span
+              style={{
+                fontSize: "10px",
+                position: "absolute",
+                transform: "translateX(-50%)",
+              }}
+            >
+              Click for
+            </span>
+            <br />
+            Next Theme
+          </button>
+        </div>
       </div>
     </>
   );
