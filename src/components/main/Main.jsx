4ef9f01a473ba8f71d2ffdc43fe00e9216ce5976
NatalyHelps <125290818+NatalyHelps@users.noreply.github.com>
2024-07-07 14:11:37 -0500

    full border theme options added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import "./Main.css";
 import Portrait from "./Portrait";
 import HomeText from "./HomeText";
+import SideBorders from "./SideBorders";
 
 const Main = () => {
   return (
     <div id="main">
       <div id="home-content">
+        <SideBorders />
         <Portrait />
         <HomeText />
       </div>
