405da4368d0bcf8f7b930cdc95a936d1a042d2d7
NatalyHelps <125290818+NatalyHelps@users.noreply.github.com>
2024-07-07 02:19:02 -0500

    theme selector working

--- src/components/main/ThemeSelector.jsx
+++ src/components/main/ThemeSelector.jsx
@@ -16,9 +16,12 @@ function ThemeSelector() {
         <select value={theme} onChange={(e) => selectTheme(e.target.value)}>
           <option value="light">Light</option>
           <option value="dark">Dark</option>
-          <option value="no-bars">No Bars</option>
-          <option value="full-bars">Full Bars</option>
-          <option value="full-border">Full Border</option>
+          <option value="no-bars-dark">No Bars Dark</option>
+          <option value="no-bars-light">No Bars Light</option>
+          <option value="full-bars-dark">Full Bars Dark</option>
+          <option value="full-bars-light">Full Bars Light</option>
+          <option value="full-border-dark">Full Border Dark</option>
+          <option value="full-border-light">Full Border Light</option>
         </select>
       </div>
     </>
