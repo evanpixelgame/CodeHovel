780b551af6ce3a5c322286f5d2ad9b9c189b5fc0
evanpixelgame <evanpixelgame@gmail.com>
2024-07-16 00:35:22 -0400

    safe redeploy :D

--- src/ThemeCustomizer/utils/handleInputChangeWithLogging.js
+++ src/ThemeCustomizer/utils/handleInputChangeWithLogging.js
@@ -1,4 +1,6 @@
 
+//may delete the logging version after testing and development
+
 const handleInputChangeWithLogging = (handleInputChange) => (property, value) => {
   console.log(`${property} updated to:`, value);
   handleInputChange(property, value);
