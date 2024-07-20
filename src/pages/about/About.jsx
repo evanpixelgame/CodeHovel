6e44cfdf2f0985cc74480e49df8a9da94b1e0777
evanpixelgame <evanpixelgame@gmail.com>
2024-07-20 17:36:40 -0400

    pregalleryfixsave

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,12 +3,14 @@ import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import ThemeCustomizer from "../../components/ThemeCustomizer/ThemeCustomizer";
 import ShowCustomizerButton from "../../components/ThemeCustomizer/subcomponents/ShowCustomizerButton";
+import MattLogo from "../../assets/MattLogo7.svg";
 
 const About = () => {
   const [showCustomizer, setShowCustomizer] = useState(false);
   return (
     <>
       <Header />
+      <img src={MattLogo} alt="matt logo test" id="matttest" />
       <div>About</div>
       <ThemeCustomizer
         showCustomizer={showCustomizer}
