dd1298b23c7487f348613f54d2df93d9e21628c4
evanpixelgame <evanpixelgame@gmail.com>
2024-07-21 04:07:53 -0400

    basic modal dragging with interactjs

--- src/components/imageDisplay/modal/Modal.jsx
+++ src/components/imageDisplay/modal/Modal.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import interact from "interactjs";
 import "./Modal.css"; // Include your styles for the modal
 
 const stopProp = (e) => {
-  console.log(e);
-  // e.stopPropagation();
+  e.stopPropagation(); // Prevents click events from propagating to the overlay
 };
 
 const Modal = ({ src, alt, onClose }) => {
+  const modalImageRef = useRef(null);
+
+  useEffect(() => {
+    if (modalImageRef.current) {
+      interact(modalImageRef.current).draggable({
+        inertia: true,
+        modifiers: [
+          interact.modifiers.restrictRect({
+            restriction: "parent",
+            endOnly: true,
+          }),
+        ],
+        onmove: (event) => {
+          const { target } = event;
+          const x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+          const y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+          target.style.transform = `translate(${x}px, ${y}px)`;
+          target.setAttribute("data-x", x);
+          target.setAttribute("data-y", y);
+        },
+      });
+
+      interact(modalImageRef.current).gesturable({
+        onmove: (event) => {
+          const { target } = event;
+          const scale =
+            (parseFloat(target.getAttribute("data-scale")) || 1) + event.ds;
+          target.style.transform = `scale(${scale})`;
+
+          target.setAttribute("data-scale", scale);
+        },
+      });
+    }
+  }, []);
+
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={stopProp}>
-        <div className="modal-image=wrapper">
-          <img src={src} alt={alt} className="modal-image" />
+        <div className="modal-image-wrapper">
+          <img
+            src={src}
+            alt={alt}
+            className="modal-image"
+            ref={modalImageRef}
+            style={{ width: "100%", height: "auto" }}
+          />
         </div>
       </div>
     </div>
