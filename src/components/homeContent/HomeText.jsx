1a2e59f1e653e529b3d7e829bd52942aee512559
evanpixelgame <evanpixelgame@gmail.com>
2024-07-20 21:58:14 -0400

    baby bird button added to home

--- src/components/homeContent/HomeText.jsx
+++ src/components/homeContent/HomeText.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ThemeCustomizer from "../ThemeCustomizer/ThemeCustomizer";
 import ShowCustomizerButton from "../ThemeCustomizer/subcomponents/ShowCustomizerButton";
 
@@ -14,6 +15,18 @@ const HomeText = ({ showCustomizer, setShowCustomizer }) => {
         showCustomizer={showCustomizer}
         setShowCustomizer={setShowCustomizer}
       />
+      <Link to="/projects/imageDisplay">
+        <button
+          type="button"
+          className="theme-button"
+          id="baby-bird-button"
+          style={{ margin: "10px", backgroundColor: "lightcoral" }}
+        >
+          <h1 style={{ fontSize: "15px" }}>
+            BABY BIRD <br /> VIDEOS <br /> BUTTON
+          </h1>
+        </button>
+      </Link>
     </div>
   );
 };
